f4df5d5d57579f5faa4808aeed64208799cb96fa
not_joon <lbj199874@gmail.com>
2023-07-26 15:06:33 +0900

    basic repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::{self, Write};
+
+use ansi_term::Color;
+use buck::engine::BuckDB;
+
 fn main() {
-    println!("Hello, world!");
+    let mut db = BuckDB::new();
+
+    loop {
+        println!("Enter 'exit' to quit.");
+        print!("{} ", Color::Green.paint("buck>"));
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let input = input.trim();
+
+        if input == "exit" {
+            break;
+        }
+
+        println!("Unrecognized command '{}'.", input)
+    }
 }
